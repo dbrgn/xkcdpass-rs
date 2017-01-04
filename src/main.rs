f106875b3dd9c38374f496a635dbfe9f9862999e
Danilo Bargen <mail@dbrgn.ch>
2017-01-04 18:38:51 +0100

    Add support for command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,32 @@
+extern crate docopt;
 extern crate rand;
+extern crate rustc_serialize;
 #[macro_use] extern crate lazy_static;
 
 use rand::distributions::{IndependentSample, Range};
+use docopt::Docopt;
+
+const USAGE: &'static str = "
+Generate XKCD style passwords.
+
+Usage:
+  xkcdpass [-c <count>]
+
+Options:
+  -h --help     Show this screen.
+  -v --version  Show version.
+  -c <count>    Number of words [default: 4].
+";
 
 static WORDLIST: &'static str = include_str!("wordlist.txt");
 lazy_static! {
     static ref MAX_OFFSET: usize = WORDLIST.trim().chars().filter(|c| *c as u32 == 0x0a).count();
     static ref BETWEEN: Range<usize> = Range::new(0, *MAX_OFFSET);
+}
+
+#[derive(Debug, RustcDecodable)]
+struct Args {
+    flag_c: usize,
 }
 
 fn get_random_word<'a>() -> &'a str {
@@ -16,7 +36,11 @@ fn get_random_word<'a>() -> &'a str {
 }
 
 fn main() {
-    let count = 5;
+    let args: Args = Docopt::new(USAGE)
+                            .and_then(|d| d.decode())
+                            .unwrap_or_else(|e| e.exit());
+
+    let count = args.flag_c;
     for _ in 0..count-1 {
         print!("{} ", get_random_word());
     }
