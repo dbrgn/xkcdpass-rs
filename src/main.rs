223c5bb571236929337ac038d9cd5c602f447f81
pengowen123 <pengowen816@gmail.com>
2017-01-05 15:02:17 -0700

    Implement external wordlist option (#6)
    * Allow users to use external wordlists
    
    To use it, call like: `xkcdpass -w <wordlist>`
    Multiple `-w` arguments will concatenate all provided lists
    
    * Remove lazy_static dependency
    
    * Fix -h and -v options not working
    
    * Style changes and optimizations
    
    * Remove local file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 extern crate docopt;
 extern crate rand;
 extern crate rustc_serialize;
-#[macro_use] extern crate lazy_static;
 
-use std::process;
+use std::{process, io, path};
+use std::fs::File;
+use std::io::Read;
 use rand::distributions::{IndependentSample, Range};
 use docopt::Docopt;
 
@@ -11,42 +12,74 @@ const USAGE: &'static str = "
 Generate XKCD style passwords.
 
 Usage:
-  xkcdpass [-c <count>]
+  xkcdpass [options] [-c <count>] [-w <list>...]
 
 Options:
-  -h --help     Show this screen.
-  -v --version  Show version.
-  -c <count>    Number of words [default: 4].
+  -h --help                  Show this screen.
+  -v --version               Show version.
+  -c <count>                 Number of words [default: 4].
+  -w --use-wordlists <list>  Wordlist to use.
 ";
 
-static WORDLIST: &'static str = include_str!("wordlist.txt");
-lazy_static! {
-    static ref MAX_OFFSET: usize = WORDLIST.trim().chars().filter(|c| *c as u32 == 0x0a).count();
-    static ref BETWEEN: Range<usize> = Range::new(0, *MAX_OFFSET);
-}
+static DEFAULT_WORDLIST: &'static str = include_str!("wordlist.txt");
 
 #[derive(Debug, RustcDecodable)]
 struct Args {
     flag_c: usize,
+    flag_w: Option<Vec<String>>,
 }
 
-fn get_random_word() -> &'static str {
+fn load_wordlist(path: String) -> io::Result<String> {
+    let path = path::Path::new(&path);
+    let mut file = File::open(path)?;
+    let mut string = String::new();
+    file.read_to_string(&mut string)?;
+    Ok(string)
+}
+
+fn load_wordlists(paths: Vec<String>) -> io::Result<Vec<String>> {
+    paths.into_iter()
+        .map(|p| load_wordlist(p))
+        .collect::<Result<Vec<_>, _>>()
+}
+
+fn get_random_word<'a>(lines: &[&'a str], max_offset: usize) -> &'a str {
     let mut rng = rand::os::OsRng::new().unwrap_or_else(|e| {
         println!("Could not initialize random number generator: {}", e);
         process::exit(1);
     });
-    let offset = BETWEEN.ind_sample(&mut rng);
-    WORDLIST.lines().nth(offset).expect(&format!("Invalid offset: {}", offset))
+    let between = Range::new(0, max_offset);
+    let offset = between.ind_sample(&mut rng);
+    lines.get(offset).expect(&format!("Invalid offset: {}", offset))
 }
 
 fn main() {
     let args: Args = Docopt::new(USAGE)
-                            .and_then(|d| d.decode())
-                            .unwrap_or_else(|e| e.exit());
+        .and_then(|d| d.decode())
+        .unwrap_or_else(|e| e.exit());
+
+    let lists = if let Some(lists) = args.flag_w {
+        load_wordlists(lists).unwrap_or_else(|e| {
+            println!("{}", e);
+            process::exit(1);
+        })
+    } else {
+        vec![DEFAULT_WORDLIST.to_string()]
+    };
+    let lines = lists.iter().flat_map(|l| l.lines()).collect::<Vec<_>>();
+
+    let max_offset = lists.iter()
+        .map(|l| {
+            l.trim()
+                .chars()
+                .filter(|c| *c as u32 == 0x0a)
+                .count()
+        })
+        .sum();
 
     let count = args.flag_c;
     for _ in 1..count {
-        print!("{} ", get_random_word());
+        print!("{} ", get_random_word(&lines, max_offset));
     }
-    println!("{}", get_random_word());
+    println!("{}", get_random_word(&lines, max_offset));
 }
